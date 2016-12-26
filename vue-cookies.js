526efc85d352ac963145a55b1bffc84182c8b448
cmp-cc <baiwoyidao@163.com>
2016-12-26 18:08:32 +0800

    vue-cookie@0.2

--- vue-cookies.js
+++ vue-cookies.js
@@ -1,5 +1,5 @@
 /**
- * Vue Cookies v0.1
+ * Vue Cookies v1.0
  * https://github.com/cmp-cc/vue-cookies
  *
  * Copyright 2016, cmp-cc
@@ -13,24 +13,42 @@
       Vue.prototype.$cookies = this
       Vue.cookies = this
     },
-    set: function(name, value, expireDays) {
-      var exDate = new Date()
-      exDate.setDate(exDate.getDate() + expireDays)
-      document.cookie =
-                name + '=' + encodeURIComponent(value) +
-                ((expireDays) == null ? '' : ';expires='+ exDate.toGMTString())
-    },
+    // ===
 
-    get: function( key ){
-      if(document.cookie.length > 0) {
-        var nameIndex = document.cookie.indexOf(key + '=')
-        if(nameIndex !== -1){
-          var nameStartIndex = nameIndex + key.length + 1
-          var nameEndIndex = document.cookie.indexOf(';',nameStartIndex)
-          return decodeURIComponent(document.cookie.substring(nameStartIndex,nameEndIndex))
+    get: function (key) {
+      return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    },
+    set : function (key, value, expireDays, path, domain, secure) {
+      if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) { return false; }
+      var sExpires = "";
+      if (expireDays) {
+        switch (expireDays.constructor) {
+          case Number:
+            sExpires = expireDays === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + expireDays;
+            break;
+          case String:
+            sExpires = "; expires=" + expireDays;
+            break;
+          case Date:
+            sExpires = "; expires=" + expireDays.toUTCString();
+            break;
         }
       }
-      return ''
+      document.cookie = encodeURIComponent(key) + "=" + encodeURIComponent(value) + sExpires + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "") + (secure ? "; secure" : "");
+      return true;
+    },
+    remove: function (key, path, domain) {
+      if (!key || !this.hasItem(key)) { return false; }
+      document.cookie = encodeURIComponent(key) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + ( domain ? "; domain=" + domain : "") + ( path ? "; path=" + path : "");
+      return true;
+    },
+    hasItem: function (key) {
+      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    },
+    keys: /* optional method: you can safely remove it! */ function () {
+      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+      for (var nIdx = 0; nIdx < aKeys.length; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+      return aKeys;
     }
   }
 
@@ -39,7 +57,6 @@
   } else if (typeof define == "function" && define.amd) {
     define([], function(){ return VueCookies; })
   } else if (window.Vue) {
-    window.VueCookies = VueCookies;
     Vue.use(VueCookies);
   }
 
