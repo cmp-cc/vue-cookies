29efe83274150b2b003c0d0347910a2cfe9aa958
cmp-cc <baiwoyidao@sina.com>
2018-12-08 19:13:37 +0800

    fix v1.5.9 build error

--- vue-cookies.js
+++ vue-cookies.js
@@ -32,7 +32,7 @@
         get: function(key) {
             var value = decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null
 
-            if(value && value.substring(0,1) === "{" && value.substring(value.length-1,value.length === "}") {
+            if(value && value.substring(0,1) === "{" && value.substring(value.length-1,value.length === "}")) {
                 try {
                     value = JSON.parse(value)
                 }catch (e) {
