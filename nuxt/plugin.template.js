191f980bcb9ae767f10d58584392bc3137e6dfdd
mean-cj <register@ireallyhost.com>
2020-04-21 16:04:23 +0700

    implement with nuxt module
    - implement with nuxt module
    - add vue plugin option
    - version 1.7.1

---
+++ nuxt/plugin.template.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueCookies from 'vue-cookies'
+
+export default (ctx, inject) => {  
+
+  // inject options from module
+  const pluginOptions = [<%= serialize(options) %>][0] || {}
+  const property = pluginOptions.property || '$cookies'
+
+  Vue.use(VueCookies, pluginOptions)
+  const instance = Vue.prototype[property]
+    
+  if (instance) {
+    // ctx[property] = instance
+    inject(property.replace('$',''), instance)
+  }
+}
